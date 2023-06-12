06c68622ea0ca86e7141073ccfa9bef0c8641026
Muhammad Adeel <91562562+madeel7258@users.noreply.github.com>
2023-06-12 11:18:58 +0500

    file updated

---
+++ calculator.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "Enter the sedoned number: ";
+    cin >> num1;
+
+    cout << "Enter an operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter the second number: ";
+    cin >> num2;wemngkfweklnmlgmwewe
+    f
+    we
+    g
+    
+    ear
+    ger
+    g
+    r
+    
+
+    double result;
+    bool isValid = true;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                cout << "Error: Division by zero is not allowed.";
+                isValid = false;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operation.";
+            isValid = false;
+            break;
+    }
+
+    if (isValid)
+        cout << "Result: " << num1 << " " << operation << " " << num2 << " = " << result;
+
+    return 0;
+}
